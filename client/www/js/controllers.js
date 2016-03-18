b1646f7a1349f328a60d457a26c2bbf5ae230134
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-03-18 10:18:20 +0530

    ClientSide: Stubbed server side data, to perform update scoreboard operations.

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+  angular.module('starter.controllers', [])
 
 .controller('MemberCtrl', function($scope, $ionicModal, $timeout, $http, $ionicPopup, ionicMaterialInk) {
 // Form data for the login modal
@@ -56,7 +56,9 @@ angular.module('starter.controllers', [])
         }
       }).error(function(user, error) {
       // The login failed. Check error to see why.
-        $scope.showAlert('No Active Connection', 'Connection to the server is Severed')
+        //$scope.showAlert('No Active Connection', 'Connection to the server is Severed')
+        $scope.loginData.loginSuccess = true;
+        $scope.modal.hide();
       });
   };
   $scope.logout = function(){
@@ -65,7 +67,7 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('RegisterMatchCtrl', function($scope, $http, $ionicPopup, ionicMaterialInk) {
+.controller('RegisterMatchCtrl', function($scope, $http, $ionicPopup, ionicMaterialInk, $timeout) {
   $scope.sport_list = ['cricket', 'basketball', 'football'];
   $scope.match_details = {
     "attributes": {"teamsList": [{}, {}]}
@@ -90,7 +92,97 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('UpdateScoreBoardCtrl', function($scope) {
+.controller('UpdateScoreBoardCtrl', function($scope, $http, ionicMaterialInk, $ionicPopup, $timeout, $ionicLoading) {
+  $scope.score = {}
+  $scope.increment = {
+    'extras': 0,
+    'wickets': 0,
+    'runs': 0
+  }
+    /** Shows the loading screen */
+  $scope.showLoadingScreen = function() {
+    $ionicLoading.show();
+  };
+
+  /** Hides the loading screen */
+  $scope.hideLoadingScreen = function(){
+    $ionicLoading.hide();
+  };
+  $scope.showAlert = function(title, msg) {
+    var alertPopup = $ionicPopup.alert({
+      title: title,
+      template: msg
+    });
+    $timeout(function() {
+      // Ionic material animation
+      ionicMaterialInk.displayEffect()
+    }, 500);
+  };
+  $scope.getMatchList = function(){
+    var serverUrl = "http://localhost:5000/getMatchList";
+    $http.get(serverUrl).success(function(response){
+      $scope.matchList = response;
+    }).error(function(data){
+      //$scope.showAlert('No Active Connection', 'Connection to the server is Severed')
+      $scope.matchList = [
+        {'id': 12, 'title': 'M1'},
+        {'id': 23, 'title': 'M2'}
+      ];
+    });
+  }
+  $scope.updateMatch = function(){
+    var id = $scope.score.currentMatch.id;
+    if(id !== undefined){
+      if($scope.score[id] == undefined){
+        var serverUrl = 'http://localhost:5000/getMatchDetails/%id'
+        serverUrl.replace('%id', id);
+        $scope.showLoadingScreen();
+        $http.get(serverUrl).success(function(response){
+          $scope.score[id] = response;
+          $scope.currentMatch = response;
+          $scope.hideLoadingScreen();
+      }).error(function(data){
+          $scope.hideLoadingScreen();
+          $scope.currentMatch = {'wickets':3, 'runs': 7, 'extras': 12};
+          $scope.score[id] = $scope.currentMatch;
+          //$scope.showAlert('No Active Connection', 'Connection to the server is Severed');
+        })
+      }
+      else{
+        $scope.currentMatch = $scope.score[id];
+        $scope.increment = {
+            'wickets': 0,
+            'runs': 0,
+            'extras': 0,
+          }
+      }
+    }
+  }
+  $scope.updateScore = function(){
+    var id = $scope.score.currentMatch.id;
+    console.log($scope.increment);
+    var serverUrl = 'http://localhost:5000/submitMatchDetails'
+    $scope.showLoadingScreen();
+    $http.post(serverUrl, $scope.increment).success(function(response){
+      $scope.hideLoadingScreen();
+      $scope.score[id].wickets += $scope.increment.wickets;
+      $scope.score[id].runs += $scope.increment.runs;
+      $scope.score[id].extras += $scope.increment.extras;
+    }).error(function(data){
+      $scope.hideLoadingScreen();
+      $scope.showAlert('No Active Connection', 'Connection to the server is Severed');
+      $scope.score[id].wickets += $scope.increment.wickets;
+      $scope.score[id].runs += $scope.increment.runs;
+      $scope.score[id].extras += $scope.increment.extras;
+    })
+  }
+  $scope.$watch(
+    function(){return $scope.loginData.loginSuccess},
+    function(newValue, oldValue){
+      if(newValue === true){
+        $scope.getMatchList();
+      }
+    }, true);
 })
 
 .controller('SportCtrl', function($scope) {
